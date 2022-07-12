933240cc3891d45bdf15edf3e0c54203fffaacf9
AnkitSingh60 <ankitanwar730@gmail.com>
2022-07-12 13:49:15 +0530

    added basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,29 @@ import { useState } from 'react';
 function App() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-  
+  const [loading, setLoading] = useState(false);
+
+
   return (
     <div className="App">
-      <h1>hey kiddo</h1>
+      <h1 className="heading">Gifs</h1>
+      <div className='search'>
+        <input type="text" placeholder="Search"  />
+        <button>Search</button>
+      </div>
+      <div className='result'>
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      <img className='gifs' src="https://media0.giphy.com/media/j4fbBhYgu8mNEHkQ4w/giphy.gif" alt="gifs" />
+      </div>
+
+
+
     </div>
   );
 }
