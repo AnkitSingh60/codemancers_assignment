5b1029ae575c028180102a25091ff2716d031648
AnkitSingh60 <ankitanwar730@gmail.com>
2022-07-12 13:36:39 +0530

    react app created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  
+  return (
+    <div className="App">
+      <h1>hey kiddo</h1>
+    </div>
+  );
+}
+
+export default App;
